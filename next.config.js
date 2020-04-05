36bf7b39b6191c8536cbbd9cd2619ad2a2cc88ca
Brandon Martinez <me@brandonmartinez.com>
2020-04-05 23:23:12 +0000

    Merged PR 10: Pulling Azure DevOps BUILD Id variable.
    Pulling Azure DevOps BUILD Id variable.
    
    Related work items: #38

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
 	env: {
-		buildId: 'local'
+		buildId: (process.env.BUILD_BUILDID || 'local').replace(
+			'brandonmartinez_',
+			''
+		)
 	},
 	webpack: function (config, { isServer }) {
 		// Fix for using fs
